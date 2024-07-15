05d84453fc1952d589cb68539c568c681fe21732
RedCommander735 <40245965+RedCommander735@users.noreply.github.com>
2024-07-16 00:59:48 +0200

    feat: first layer of apptree

--- src/debugger.js
+++ src/debugger.js
@@ -1,24 +1,46 @@
 document.body.style.border = "5px solid green";
-
-const webframe = XPCNativeWrapper.unwrap(window).webframe;
-
-let initialised = false
-
+// @ts-ignore
+const frame = XPCNativeWrapper.unwrap(window).webframe;
 function init() {
     let interval = setInterval(() => {
-        if (XPCNativeWrapper.unwrap(window).webframe.getMainApp) {
-            if (XPCNativeWrapper.unwrap(window).webframe.getMainApp().isInitComplete) {
-                initialised = true
-                done()
-                clearInterval(interval)
+        if (frame.getMainApp) {
+            if (frame.getMainApp().isInitComplete) {
+                done();
+                clearInterval(interval);
             }
         }
-    }, 500)
+    }, 500);
 }
-
 function done() {
     // Implement debugger here
+    let appTree = {};
+    const mainAppDef = frame.getMainApp().appDef;
+    const mainAppName = mainAppDef.appName;
+    const mainAppAlias = mainAppDef.appAlias;
+    appTree[mainAppName] = {
+        appAlias: mainAppAlias,
+        subApps: (mainAppDef.subAppsRef) ? {} : null
+    };
+    const subAppRefs = Object.keys(mainAppDef.subAppsRef);
+    getSubTree(appTree[mainAppName].subApps, mainAppDef);
     console.log('Done');
+    console.log(appTree);
 }
-
-init()
+function getSubTree(subAppTree, currentAppDef) {
+    // TODO Zweite Ebene
+    const subAppRefs = Object.keys(currentAppDef.subAppsRef);
+    for (let ref of subAppRefs) {
+        try {
+            let subAppDef = frame.getMainApp().getSubAppDef(ref);
+            subAppTree[subAppDef.appName] = {
+                appAlias: subAppDef.appAlias,
+                subApps: (subAppDef.subAppsRef) ? {} : null
+            };
+            if (subAppDef.subAppsRef) {
+                getSubTree(subAppTree[subAppDef.appName].subApps, subAppDef);
+            }
+        }
+        catch { }
+    }
+}
+init();
